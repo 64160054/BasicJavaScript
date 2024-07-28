2db0d2bdc2b504ad6e956f828e55aa6d564d5f4b
Paowaric <64160054@go.buu.ac.th>
2024-07-28 23:54:11 +0700

    test pro

--- script.js
+++ script.js
@@ -1,33 +1,35 @@
-// ขึ้นใน Console ตอน F12
-console.log('Hello World'); 
+// ! ขึ้นใน Console ตอน F12
+// console.log('Hello World'); 
 
-// document(index.html) get id มาจาก p ชื่อว่า text และใช้ innerHTML เพื่อ set ค่าที่จะแสดง
-document.getElementById('text').innerHTML = "Paowaric Vachiranantakul" 
+// ! document(index.html) get id มาจาก p ชื่อว่า text และใช้ innerHTML เพื่อ set ค่าที่จะแสดง
+// document.getElementById('text').innerHTML = "Paowaric Vachiranantakul" 
 
-// แสดงข้อความเหมือนกัน
-document.write('Hello Document Write'); 
+// ! แสดงข้อความเหมือนกัน
+// document.write('Hello Document Write'); 
 
-// ขึ้น Pop up กล่อง Alert ตอนเข้า test
+// ! ขึ้น Pop up กล่อง Alert ตอนเข้า test
 // window.alert('Hello Alert Box'); 
 
-//สร้างตัวแปร var เสมอ 
-var x = 5; 
-var y = 6;
-var z = x + y;
-console.log(z);
+// ! สร้างตัวแปร var เสมอ 
+// var x = 5; 
+// var y = 6;
+// var z = x + y;
+// console.log(z);
 
-//Concatenate การเชื่อมต่อ String หรือ Variable
-var FirstName = "Paowaric";
-var LastName = "Vachiranantakul";
-var FullName = FirstName + " " + LastName;
-console.log(FullName);
+// ! Concatenate การเชื่อมต่อ String หรือ Variable
+// var FirstName = "Paowaric";
+// var LastName = "Vachiranantakul";
+// var FullName = FirstName + " " + LastName;
+// console.log(FullName);
 
-//True
-console.log(1==1);
+// ! True
+// console.log(1==1);
 
-//Function
-function myFunction(value1, value2) {
-    return value1*value2;
-}
+// ! Function
+// function myFunction(value1, value2) {
+//     return value1*value2;
+// }
 
-console.log(myFunction(5, 5));
+// console.log(myFunction(5, 5));
+
+test
