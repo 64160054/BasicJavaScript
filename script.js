5f958e61eab7a09d033c7266b4203f540ffe5a12
Paowaric <Gxz321@hotmail.com>
2024-07-28 23:20:15 +0700

    output & variable

---
+++ script.js
@@ -0,0 +1,17 @@
+// ขึ้นใน Console ตอน F12
+console.log('Hello World'); 
+
+// document(index.html) get id มาจาก p ชื่อว่า text และใช้ innerHTML เพื่อ set ค่าที่จะแสดง
+document.getElementById('text').innerHTML = "Paowaric Vachiranantakul" 
+
+// แสดงข้อความเหมือนกัน
+document.write('Hello Document Write'); 
+
+// ขึ้น Pop up กล่อง Alert ตอนเข้า test
+window.alert('Hello Alert Box'); 
+
+//สร้างตัวแปร var เสมอ 
+var x = 5; 
+var y = 6;
+var z = x + y;
+console.log(z);
